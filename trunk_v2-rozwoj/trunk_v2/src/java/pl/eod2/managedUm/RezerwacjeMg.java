120a75af907607e8556c9699a92092f28fdfe920
arti01 <arti01@stacja.lan>
2016-03-15 09:18:13 +0100

    prace na lista uczestnikow

--- trunk_v2-rozwoj/trunk_v2/src/java/pl/eod2/managedUm/RezerwacjeMg.java
+++ trunk_v2-rozwoj/trunk_v2/src/java/pl/eod2/managedUm/RezerwacjeMg.java
@@ -1,6 +1,7 @@
 package pl.eod2.managedUm;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -17,6 +18,7 @@ import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.ScheduleModel;
 import org.primefaces.model.TreeNode;
 import pl.eod.abstr.AbstMg;
+import pl.eod.encje.Uzytkownik;
 import pl.eod.managed.Login;
 import pl.eod2.encje.UmGrupa;
 import pl.eod2.encje.UmMasterGrupa;
@@ -29,9 +31,9 @@ import pl.eod2.encje.exceptions.NonexistentEntityException;
 @ManagedBean(name = "RezerwacjeMg")
 @SessionScoped
 public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @ManagedProperty(value = "#{login}")
     private Login login;
     @ManagedProperty(value = "#{UrzadzeniaMg}")
@@ -42,24 +44,32 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
     private TreeNode urzadzenie;
     private ScheduleModel eventModel;
     private DefaultScheduleEvent event = new DefaultScheduleEvent();
-    
+    private List<Uzytkownik> usersList;
+
     public RezerwacjeMg() throws InstantiationException, IllegalAccessException {
         super("/um/rezerwacje", new UmRezerwacjeKontr(), new UmRezerwacje());
     }
-    
+
     @PostConstruct
     private void init() {
         eventModel = new DefaultScheduleModel();
+        usersList = new ArrayList<>();
+        for (Uzytkownik u : login.getZalogowany().getUserId().getSpolkaId().getUserList()) {
+            usersList.add(u);
+        }
+        /*login.getZalogowany().getUserId().getSpolkaId().getUserList().stream().filter((u) -> (!u.getStruktura().isUsuniety())).forEach((u) -> {
+            usersList.add(u);
+        });*/
         zrobDrzewo();
     }
-    
+
     @Override
     public void refresh() throws InstantiationException, IllegalAccessException {
         super.refresh();
         login.refresh();
         zrobDrzewo();
     }
-    
+
     public void zrobDrzewo() {
         root = new DefaultTreeNode("urządzenia", null);
         List<UmMasterGrupa> masterList = login.getZalogowany().getUserId().getSpolkaId().getUmMasterGrupaList();
@@ -79,7 +89,7 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
             }
         }
     }
-    
+
     public void ustawSched() {
         if (!this.urzadzenie.getType().equals("urzadzenie")) {
             return;
@@ -93,7 +103,7 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
             eventModel.addEvent(ev);
         }
     }
-    
+
     public void onDateSelect(SelectEvent selectEvent) {
         event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
         obiekt = new UmRezerwacje();
@@ -101,22 +111,22 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
         obiekt.setDataOd(event.getStartDate());
         obiekt.setDataDo(event.getEndDate());
     }
-    
+
     public void onEventSelect(SelectEvent selectEvent) {
         event = (DefaultScheduleEvent) selectEvent.getObject();
         obiekt = obiekt = dcC.findObiekt(((UmRezerwacje) event.getData()).getId());
     }
-    
+
     public void onEventMove(ScheduleEntryMoveEvent selectEvent) throws NonexistentEntityException, Exception {
         DefaultScheduleEvent oldEvent = (DefaultScheduleEvent) selectEvent.getScheduleEvent();
         ruchEvent(oldEvent);
     }
-    
+
     public void onEventResize(ScheduleEntryResizeEvent selectEvent) throws NonexistentEntityException, Exception {
         DefaultScheduleEvent oldEvent = (DefaultScheduleEvent) selectEvent.getScheduleEvent();
         ruchEvent(oldEvent);
     }
-    
+
     private void ruchEvent(DefaultScheduleEvent oldEvent) throws NonexistentEntityException, Exception {
         obiekt = dcC.findObiekt(((UmRezerwacje) oldEvent.getData()).getId());
         Date stOd = obiekt.getDataOd();
@@ -128,7 +138,7 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
             oldEvent.setEndDate(stDo);
         }
     }
-    
+
     public void addEventObj() throws InstantiationException, IllegalAccessException, NonexistentEntityException, Exception {
         if (event.getId() == null) {
             obiekt.setTworca(login.getZalogowany().getUserId());
@@ -154,60 +164,83 @@ public class RezerwacjeMg extends AbstMg<UmRezerwacje, UmRezerwacjeKontr> implem
         }
         event = new DefaultScheduleEvent();
     }
-    
+
     public void delEvent(ActionEvent actionEvent) throws IllegalOrphanException, NonexistentEntityException, InstantiationException, IllegalAccessException {
         obiekt = (UmRezerwacje) event.getData();
         usun();
         refresh();
         eventModel.deleteEvent(event);
     }
-    
+
     public Login getLogin() {
         return login;
     }
-    
+
     public void setLogin(Login login) {
         this.login = login;
     }
-    
+
     public TreeNode getUrzadzenie() {
         return urzadzenie;
     }
-    
+
     public void setUrzadzenie(TreeNode urzadzenie) {
         this.urzadzenie = urzadzenie;
     }
-    
+
     public UrzadzeniaMg getUrzMg() {
         return urzMg;
     }
-    
+
     public void setUrzMg(UrzadzeniaMg urzMg) {
         this.urzMg = urzMg;
     }
-    
+
     public TreeNode getRoot() {
         return root;
     }
-    
+
     public void setRoot(TreeNode root) {
         this.root = root;
     }
-    
+
     public ScheduleModel getEventModel() {
         return eventModel;
     }
-    
+
     public void setEventModel(ScheduleModel eventModel) {
         this.eventModel = eventModel;
     }
-    
+
     public DefaultScheduleEvent getEvent() {
         return event;
     }
-    
+
     public void setEvent(DefaultScheduleEvent event) {
         this.event = event;
     }
-    
+
+    public List<Uzytkownik> getUsersList() {
+        return usersList;
+    }
+
+    public void setUsersList(List<Uzytkownik> usersList) {
+        this.usersList = usersList;
+    }
+
+    public List<Uzytkownik> dostepniList(String cos) {
+        List<Uzytkownik> wynik = new ArrayList<>();
+        System.err.println(usersList);
+        if (obiekt.getUczestnikList() == null) {
+            obiekt.setUczestnikList(new ArrayList<>());
+        }
+        //System.err.println(obiekt.getUczestnikList());
+        usersList.removeAll(obiekt.getUczestnikList());
+        for (Uzytkownik u : usersList) {
+            if (u.getAdrEmail().toLowerCase().contains(cos.toLowerCase()) || u.getFullname().toLowerCase().contains(cos.toLowerCase())) {
+                wynik.add(u);
+            }
+        }
+        return wynik;
+    }
 }
