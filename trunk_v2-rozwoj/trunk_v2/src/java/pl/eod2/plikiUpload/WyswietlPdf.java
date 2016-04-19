1cbe954be67f2e6c8b8fd35d67b06ae074176c7b
103039 <103039@L150998.centrala.bzwbk>
2016-04-19 15:02:20 +0200

    lista plikow

--- trunk_v2-rozwoj/trunk_v2/src/java/pl/eod2/plikiUpload/WyswietlPdf.java
+++ trunk_v2-rozwoj/trunk_v2/src/java/pl/eod2/plikiUpload/WyswietlPdf.java
@@ -1,16 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.eod2.plikiUpload;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import pl.eod2.encje.DcPlik;
 import pl.eod2.encje.DcPlikJpaController;
 
@@ -30,6 +30,22 @@ public class WyswietlPdf {
             Logger.getLogger(WyswietlPdf.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex1) {
             ex1.printStackTrace();
+        }
+    }
+
+    public StreamedContent getImage() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        dcPlikC = new DcPlikJpaController();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Get ID value from actual request param.
+            String id = context.getExternalContext().getRequestParameterMap().get("id");
+            DcPlik plikW = dcPlikC.findDcPlik(new Long(id).intValue());
+            byte[] image = plikW.getPlik();
+            return new DefaultStreamedContent(new ByteArrayInputStream(image));
         }
     }
 }
