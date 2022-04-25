477fcfacae424a709a19313d67eb9825402b9da8
arti01 <arti01@stacja.lan>
2022-04-25 09:29:04 +0200

    kube1

---
+++ kube1/src/main/java/artiaaaa/kube1/Kube1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package artiaaaa.kube1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author arti01
+ */
+@SpringBootApplication
+public class Kube1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kube1.class, args);
+    }
+}
